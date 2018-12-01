d3098e5153c41bdf1d8eeba5bc0d28cec1820aec
LitsItSchool <38472702+LitsItSchool@users.noreply.github.com>
2018-12-01 23:37:51 +0200

    Unity3d Physics lesson
    Unity3d Physics lesson

---
+++ Assets/Addforcer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Addforcer : MonoBehaviour {
+    public Rigidbody rigidbody;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.W))
+        {
+            rigidbody.AddForce(Vector3.up, ForceMode.Impulse);
+        }
+	}
+}
---
+++ Assets/CharC.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharC : MonoBehaviour {
+
+    
+    public CharacterController characterController;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //   characterController.SimpleMove(Vector3.forward);
+        characterController.Move(Vector3.forward);
+    }
+}
